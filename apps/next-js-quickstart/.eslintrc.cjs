458169b0bbdf988857411d9291df474b6c749ed6
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2023-04-29 14:46:48 +0300

    Module next-js-quickstart is now part of the monorepo

--- apps/next-js-quickstart/.eslintrc.cjs
+++ apps/next-js-quickstart/.eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const eslintrcConfig = {
   root: true,
-  extends: ["next/core-web-vitals"],
+  extends: ["custom", "next/core-web-vitals"],
 };
 
 module.exports = eslintrcConfig;
