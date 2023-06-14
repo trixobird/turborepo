187ca721139b2e1cf2d56ff0540801785717af8d
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2023-06-14 10:09:32 +0300

    Create openId GitHub connect provider

--- infrastructure/modules/iam/github.tf
+++ infrastructure/modules/iam/github.tf
@@ -29,3 +29,13 @@ data "aws_iam_policy_document" "github_assume_role_policy" {
     }
   }
 }
+
+resource "aws_iam_openid_connect_provider" "github_openid_connect_provider" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
